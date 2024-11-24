35ababb0ff7627aa3c035fd072944e83ce5b5f44
xiaoka <11076632@qq.com>
2024-11-24 21:17:17 +0800

    feat: complete tupleLength

--- FirstOfArray/temeplate.ts
+++ FirstOfArray/temeplate.ts
@@ -2,3 +2,6 @@
 // type First<T extends any[]> = T['length'] extends 0 ? never : T[0] 
 // 解法2. 利用extends
 type First<T extends any[]> = T extends [] ? never : T[0]
+
+type arr = [11,23,4,4]
+type a = arr[number]
